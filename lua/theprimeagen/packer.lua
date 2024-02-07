0d2b8980c4e5baaff968ed2a6ba16f5cc77bf7ed
ftimoo <timo-faes@live.be>
2024-02-07 03:58:50 +0100

    setup 2024

---
+++ lua/theprimeagen/packer.lua
@@ -0,0 +1,48 @@
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+
+	use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use({ 'kepano/flexoki-neovim', as = 'flexoki'
+})
+
+use {
+	'nvim-treesitter/nvim-treesitter',
+	run = function()
+		local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+		ts_update()
+	end,}
+	use {
+		"theprimeagen/harpoon",
+		branch = "harpoon2",
+		requires = { {"nvim-lua/plenary.nvim"} }
+	}
+	use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  requires = {
+    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+     {'williamboman/mason.nvim'},
+     {'williamboman/mason-lspconfig.nvim'},
+
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
+  }
+}	
+end)
+
+
---
+++ lua/theprimeagen/remap.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = ' '
+
+vim.api.nvim_set_keymap('n', '<leader>pv', ':Ex<CR>', { noremap = true, silent = true })
+
